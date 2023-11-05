99f78873a7150c0f1b2a39f777e92f7b830f50c5
R3enox <129979133+R3enox@users.noreply.github.com>
2023-11-05 17:41:26 +0200

    state machine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,51 +6,53 @@ import { PixabayApi } from './helpers/pixabay-api';
 import { LoadMoreBtn } from './LodaMoreBtn/LoadMoreBtn';
 import { Loader } from './Loader/Loader';
 
+const STATUS = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
 export const App = () => {
   const [photos, setPhotos] = useState([]);
   const [searchQ, setSearchQ] = useState('');
   const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadMore, setLoadMore] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [loadMore, setLoadMore] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
+  const [status, setStatus] = useState(STATUS.IDLE);
+
+  const { PENDING, RESOLVED, REJECTED } = STATUS;
 
   useEffect(() => {
     if (searchQ || page !== 1) {
       const getPhotos = async () => {
-        setIsLoading(true);
-
+        setStatus(PENDING);
         const pixabayApi = new PixabayApi(12);
 
         pixabayApi.q = searchQ;
         pixabayApi.page = page;
-
-        if (!searchQ) {
-          return Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        }
-
         try {
           const { totalHits, hits } = await pixabayApi.getContent();
 
           if (totalHits === 0) {
-            return Notify.failure(
+            Notify.failure(
               'Sorry, there are no images matching your search query. Please try again.'
             );
+            return;
           }
+          setStatus(RESOLVED);
           Notify.success(`Hooray! We found ${totalHits} images.`);
           setPhotos(prevState => [...prevState, ...hits]);
           setTotalPages(Math.ceil(totalHits / 12));
         } catch (error) {
           Notify.failure(`${error}`);
-        } finally {
-          setIsLoading(false);
-          setLoadMore(true);
+          setStatus(REJECTED);
         }
       };
       getPhotos();
     }
-  }, [searchQ, page]);
+  }, [searchQ, page, PENDING, RESOLVED, REJECTED]);
 
   const handleSearch = searchQ => {
     setPhotos([]);
@@ -60,19 +62,84 @@ export const App = () => {
 
   const loadMorePhotos = () => {
     setPage(prevState => prevState + 1);
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
   };
 
-  return (
-    <>
-      <Search handleSearch={handleSearch} />
-      {isLoading && <Loader />}
-      {searchQ !== null && <ImageGallery photos={photos} searchQ={searchQ} />}
-      {loadMore && page !== totalPages && (
-        <LoadMoreBtn loadMore={loadMorePhotos} />
-      )}
-    </>
-  );
+  // useEffect(() => {
+  //   if (searchQ || page !== 1) {
+  //     const getPhotos = async () => {
+  //       setIsLoading(true);
+
+  //       const pixabayApi = new PixabayApi(12);
+
+  //       pixabayApi.q = searchQ;
+  //       pixabayApi.page = page;
+
+  //       if (!searchQ) {
+  //         return Notify.failure(
+  //           'Sorry, there are no images matching your search query. Please try again.'
+  //         );
+  //       }
+
+  //       try {
+  //         const { totalHits, hits } = await pixabayApi.getContent();
+
+  //         if (totalHits === 0) {
+  //           return Notify.failure(
+  //             'Sorry, there are no images matching your search query. Please try again.'
+  //           );
+  //         }
+  //         Notify.success(`Hooray! We found ${totalHits} images.`);
+  //         setPhotos(prevState => [...prevState, ...hits]);
+  //         setTotalPages(Math.ceil(totalHits / 12));
+  //       } catch (error) {
+  //         Notify.failure(`${error}`);
+  //       } finally {
+  //         setIsLoading(false);
+  //         setLoadMore(true);
+  //       }
+  //     };
+  //     getPhotos();
+  //   }
+  // }, [searchQ, page]);
+
+  // const handleSearch = searchQ => {
+  //   setPhotos([]);
+  //   setSearchQ(searchQ);
+  //   setPage(1);
+  // };
+
+  // const loadMorePhotos = () => {
+  //   setPage(prevState => prevState + 1);
+  // };
+
+  // return (
+  //   <>
+  //     <Search handleSearch={handleSearch} />
+  //     {isLoading && <Loader />}
+  //     {searchQ !== null && <ImageGallery photos={photos} searchQ={searchQ} />}
+  //     {loadMore && page !== totalPages && (
+  //       <LoadMoreBtn loadMore={loadMorePhotos} />
+  //     )}
+  //   </>
+  // );
+  if (!searchQ) return <Search handleSearch={handleSearch} />;
+
+  if (status === PENDING) {
+    return (
+      <>
+        <Search handleSearch={handleSearch} />
+        <Loader />
+      </>
+    );
+  } else if (status === RESOLVED) {
+    return (
+      <>
+        <Search handleSearch={handleSearch} />;
+        <ImageGallery photos={photos} searchQ={searchQ} />
+        {page !== totalPages && <LoadMoreBtn loadMore={loadMorePhotos} />}
+      </>
+    );
+  } else if (status === REJECTED) {
+    return <Search handleSearch={handleSearch} />;
+  }
 };
